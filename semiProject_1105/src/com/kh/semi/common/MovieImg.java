aa7f524dc06963cffddb0c6d5ebb6002372b46e5
steeveelee2 <steeveelee2@gmail.com>
2018-11-22 11:12:18 +0900

    정리함

--- semiProject_1105/src/com/kh/semi/common/MovieImg.java
+++ semiProject_1105/src/com/kh/semi/common/MovieImg.java
@@ -1,45 +1,17 @@
 package com.kh.semi.common;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.net.URLEncoder;
-
 import com.kh.semi.exception.DetailViewException;
 import com.kh.semi.user.movie.model.service.MovieService;
 
 public class MovieImg {
 
 	public String moviewImg(String keyword,String mCode) throws DetailViewException,Exception {
+		
+		String result=new PosterAPI().getPosterResult(keyword);
+		
 		MovieService dvs = new MovieService();
 		String page = "";
 		
-		String clientId = "f1lI5BaAwEDWvf6UPMiK";
-		String clientSecret = "flUGLrhsdJ";
-
-		String apiURL = "https://openapi.naver.com/v1/search/movie.json?query=" + URLEncoder.encode(keyword, "utf-8");
-
-		HttpURLConnection con = (HttpURLConnection) new URL(apiURL).openConnection();
-		con.setRequestMethod("GET");
-		con.setRequestProperty("X-Naver-Client-Id", clientId);
-		con.setRequestProperty("X-Naver-Client-Secret", clientSecret);
-
-		BufferedReader br = null;
-
-		if (con.getResponseCode() == 200)
-			br = new BufferedReader(new InputStreamReader(con.getInputStream()));
-		else
-			br = new BufferedReader(new InputStreamReader(con.getErrorStream()));
-
-		String inStr = "";
-		StringBuffer sb = new StringBuffer();
-		while ((inStr = br.readLine()) != null) sb.append(inStr);
-		String result = sb.toString();
-		System.out.println(result);
-		
-		br.close();
-
 		if (result.substring(61, 62).equals("1") && !chkNum(result.substring(62, 63)))
 			page = dvs.getImage(result,mCode);
 		else
@@ -47,6 +19,7 @@ public class MovieImg {
 		
 		return page;
 	}
+	
 	private boolean chkNum(String s) {
 		try {
 			Integer.parseInt(s);
--- semiProject_1105/src/com/kh/semi/common/MovieSmallImg.java
+++ semiProject_1105/src/com/kh/semi/common/MovieSmallImg.java
@@ -1,16 +1,5 @@
 package com.kh.semi.common;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.net.URLEncoder;
-
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
 
 import com.kh.semi.exception.DetailViewException;
 import com.kh.semi.user.movie.model.service.MovieService;
@@ -18,33 +7,10 @@ import com.kh.semi.user.movie.model.service.MovieService;
 public class MovieSmallImg{
 	
 	public String movieSmallImg(String keyword,String mCode) throws DetailViewException,Exception {
-		System.out.println("스몰 이미지");
+		String result=new PosterAPI().getPosterResult(keyword);
+		
 		MovieService dvs = new MovieService();
 		String page = "";
-		
-		String clientId = "f1lI5BaAwEDWvf6UPMiK";
-		String clientSecret = "flUGLrhsdJ";
-
-		String apiURL = "https://openapi.naver.com/v1/search/movie.json?query=" + URLEncoder.encode(keyword, "utf-8");
-
-		HttpURLConnection con = (HttpURLConnection) new URL(apiURL).openConnection();
-		con.setRequestMethod("GET");
-		con.setRequestProperty("X-Naver-Client-Id", clientId);
-		con.setRequestProperty("X-Naver-Client-Secret", clientSecret);
-
-		BufferedReader br = null;
-
-		if (con.getResponseCode() == 200)
-			br = new BufferedReader(new InputStreamReader(con.getInputStream()));
-		else
-			br = new BufferedReader(new InputStreamReader(con.getErrorStream()));
-
-		String inStr = "";
-		StringBuffer sb = new StringBuffer();
-		while ((inStr = br.readLine()) != null) sb.append(inStr);
-		String result = sb.toString();
-
-		br.close();
 
 		if (result.substring(61, 62).equals("1") && !chkNum(result.substring(62, 63)))
 			page = dvs.getSmallImage(result,mCode);
@@ -53,6 +19,7 @@ public class MovieSmallImg{
 		
 		return page;
 	}
+	
 	private boolean chkNum(String s) {
 		try {
 			Integer.parseInt(s);
